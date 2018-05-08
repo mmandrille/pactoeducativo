46c364a5d795701750e8c47802005d000cfd20fd
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-05-08 13:01:52 -0300

    Se realizaron cambios de funcionalidad basica

--- calendario/models.py
+++ calendario/models.py
@@ -7,9 +7,11 @@ from django.utils import timezone
 #Create your models here.
 class Evento(models.Model):
     nombre = models.CharField('Titulo', max_length=200)
-    descripcion = models.TextField()
-    fecha = models.DateTimeField('Fecha del Evento')
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    pub_date = models.DateTimeField('Fecha de Publicacion', default=timezone.now, editable = False)
+    descripcion = HTMLField()
+    fecha_inicio = models.DateTimeField('Fecha del Evento', default=datetime.datetime.now())
+    fecha_fin = models.DateTimeField('Fecha de fin del Evento', default=datetime.datetime.now())
+    models.ForeignKey(User, on_delete=models.CASCADE)
+    importante = models.BooleanField(default=False)
+    #ubicacion = ciudades? 
     def __str__(self):
-        return self.nombre
+        return self.nombre + ' ' + str(self.fecha_inicio)
--- calendario/views.py
+++ calendario/views.py
@@ -5,5 +5,6 @@ from .models import Evento
 
 # Create your views here.
 def calendario(request):
-    eventos = Evento.objects.order_by('fecha')
-    return render(request, 'calendario.html', { 'eventos': eventos, 'hoy' : datetime.now().strftime('%d/%m/%Y')})
+    eventos = Evento.objects.filter(fecha_inicio__date=date.today())
+    importantes = Evento.objects.filter(importante=True, fecha_inicio__date__gt=date.today())
+    return render(request, 'home.html', { 'eventos': eventos, 'importantes': importantes, 'hoy' : datetime.now().strftime('%d/%m/%Y')})
