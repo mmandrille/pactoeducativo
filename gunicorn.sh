8325849a3f344f8151d59eb154b3c322a8c6a4e8
servidor <servidor@DjangoGob.DjangoGob>
2018-05-09 13:31:11 -0300

    Se realizaron cambios para mejorar productivo.

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,5 +2,5 @@
 cd /opt/pactoeducativo
 source venv/bin/activate
 cd /opt/pactoeducativo/pactoeducativo
-gunicorn pactoeducativo.wsgi -t 600 -b 127.0.0.1:8001 -w 6 --user=servidor --group=servidor --log-file=/opt/pactoeducativo/pactoeducativo/gunicorn.log 2>>/opt/pactoeducativo/pactoeducativo/gunicorn.log
+gunicorn pactoeducativo.wsgi -t 600 -b 127.0.0.1:8001 -w 6 --user=servidor --group=servidor --log-file=/opt/pactoeducativo/gunicorn.log 2>>/opt/pactoeducativo/gunicorn.log
 
