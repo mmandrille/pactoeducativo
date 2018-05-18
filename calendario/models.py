0e2b03d8c22129df6eaf1dad07e22b352c1142c5
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-05-18 19:25:01 -0300

    Se realizaron cambios esteticos sobre varias areas.

--- calendario/models.py
+++ calendario/models.py
@@ -8,6 +8,7 @@ from tinymce.models import HTMLField
 #Create your models here.
 class Evento(models.Model):
     nombre = models.CharField('Titulo', max_length=200)
+    portada = models.FileField(upload_to='eventos/portadas/', default='/archivos/defaults/noimage.gif')
     descripcion = HTMLField()
     fecha_inicio = models.DateTimeField('Fecha del Evento', default=datetime.datetime.now())
     fecha_fin = models.DateTimeField('Fecha de fin del Evento', default=datetime.datetime.now())
--- pactoeducativo/settings.py
+++ pactoeducativo/settings.py
@@ -146,3 +146,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mmandrille@gmail.com'
 EMAIL_HOST_PASSWORD = 'comoyoquiera15'
+
+#Actualizar Statics, no solo nuevas
+AWS_PRELOAD_METADATA = True
