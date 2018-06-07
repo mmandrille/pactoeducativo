1ccda5aa09195362d0644c7a97d364dd33deb46e
Hernán Turchi <hturchi@arwebs.net>
2018-06-07 00:16:22 -0300

    Mostrar resultados de la encuesta

--- encuestas/views.py
+++ encuestas/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from .models import Encuesta, Pregunta, Opcion, Respuesta
 # Create your views here.
 def encuesta(request, encuesta_id):
-    encuesta = Encuesta.objects.get(pk=1)
+    encuesta = Encuesta.objects.get(pk=encuesta_id)
     if request.method == 'POST':
         respuesta = Respuesta()
         respuesta.encuesta = encuesta
@@ -12,6 +12,15 @@ def encuesta(request, encuesta_id):
         for pregunta in encuesta.preguntas.all():
                 respuesta.respuestas+= request.POST[str(pregunta.id)] + ','
         respuesta.save()
-        return render(request, 'encuesta_resp.html')
+
+        #Por cada opcion de cada pregunta, recolectamos la cantidad de elecciones de los distintos usuarios.
+        selecciones = []
+        for pregunta in encuesta.preguntas.all():
+            for opcion in pregunta.opciones.all():
+                match = str(pregunta.orden) + '-' + str(opcion.orden) + ','
+                matches = encuesta.respuestas.filter(respuestas__contains=match)
+                selecciones.append([pregunta.orden, opcion.orden, len(matches)])
+
+        return render(request, 'encuesta_resp.html', {'encuesta': encuesta, 'selecciones': selecciones,})
     else:
         return render(request, 'encuesta.html', {'encuesta': encuesta,})
