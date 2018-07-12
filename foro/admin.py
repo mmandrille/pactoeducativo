859c87b0046100daa330c7ff857148a1463ee8f3
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-07-12 11:33:41 -0300

    Se arreglo problema de __str__ en el models.py del foro.

--- foro/admin.py
+++ foro/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Thread, Subforo
+from .models import Thread, Subforo, Post
 
 # Register your models here.
 admin.site.register(Thread)
 admin.site.register(Subforo)
+admin.site.register(Post)
