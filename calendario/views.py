7b25724608ab00ce8408e4dd636839b869b59e1a
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-07-18 15:28:01 -0300

    Se realizo cambio para organizar calendario.

--- calendario/views.py
+++ calendario/views.py
@@ -6,6 +6,6 @@ from .models import Evento
 
 # Create your views here.
 def calendario(request):
-    eventos = Evento.objects.filter(fecha_inicio__date=date.today())
-    importantes = Evento.objects.filter(importante=True, fecha_inicio__date__gt=date.today())
+    eventos = Evento.objects.filter(fecha_inicio__date=date.today()).order_by('fecha_inicio')
+    importantes = Evento.objects.filter(importante=True, fecha_inicio__date__gt=date.today()).order_by('fecha_inicio')
     return render(request, 'calendario.html', { 'eventos': eventos, 'importantes': importantes, 'hoy' : datetime.now().strftime('%d/%m/%Y')})
