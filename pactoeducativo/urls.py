a079c2bd43ca785450100617c76886998fcf4c4a
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-12-27 02:02:05 -0300

    Se arreglo urls.py

--- pactoeducativo/urls.py
+++ pactoeducativo/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     #Standards
     path('admin/', admin.site.urls),
     url(r'^signup/$', coreviews.signup, name='signup'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^login/$',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     url(r'^tinymce/', include('tinymce.urls')),
     #FORO
     path('foro/', include('foro.urls')),
